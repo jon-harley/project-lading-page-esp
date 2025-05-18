eb2daeb6f833d26e763d66f29d65418c12b18ff9
jon-harley <jon_oliveira@outlook.com>
2025-05-18 17:49:10 -0300

    update headline

--- src/components/VSLNew.tsx
+++ src/components/VSLNew.tsx
@@ -74,7 +74,7 @@ import React, { useEffect, useRef, useState } from 'react';
   <div className="min-h-screen bg-black flex flex-col items-center px-4 py-8">
   {/* Mensagem no topo */}
   <div style={{ backgroundColor: 'red', color: 'white', textAlign: 'center', padding: '10px', width: 'fit-content', margin: '10px auto', borderRadius: '5px' }}>
-  This presentation will be taken down on {currentDate}
+  While you’re wasting time doubting, he’s already forgotten you exist. This is your last chance to be the priority in his life — miss it, and you’ll become just another memory he wants to erase.
   </div>
  
 
